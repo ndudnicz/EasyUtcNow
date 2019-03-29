218e33edcfa9406b599ea493ac373921a63f66f5
Nicolas DUDNICZENKO <ndudnicz@protonmail.com>
2019-03-29 08:25:11 +0100

    crystal tool format

--- src/crtimestamp.cr
+++ src/crtimestamp.cr
@@ -6,12 +6,11 @@ require "crystal/system/time"
 #
 
 struct Crtimestamp
-
   # :nodoc:
   NANOSECONDS_PER_MILLISECOND = 1_000_000_i64
 
   # epoch's timestamp in second since `0001-01-01 00:00:00`
-  EPOCH_SECONDS_TIMESTAMP = 62135596800_i64
+  EPOCH_SECONDS_TIMESTAMP     = 62135596800_i64
   EPOCH_SECONDS_TIMESTAMP_F64 = 62135596800_f64
 
   # epoch's timestamp in msecond since `0001-01-01 00:00:00`
@@ -19,9 +18,9 @@ struct Crtimestamp
 
   # Returns an utc epoch current timestamp in second
   #
-  #```
+  # ```
   # Time.now_utc_to_unix # => 1452567845
-  #```
+  # ```
   def self.now_utc_to_unix : Int64
     seconds, nanoseconds = Crystal::System::Time.compute_utc_seconds_and_nanoseconds
     seconds - EPOCH_SECONDS_TIMESTAMP
@@ -29,9 +28,9 @@ struct Crtimestamp
 
   # Returns an utc epoch current timestamp in msecond
   #
-  #```
+  # ```
   # Time.now_utc_to_unix_ms # => 1452567845876
-  #```
+  # ```
   def self.now_utc_to_unix_ms : Int64
     seconds, nanoseconds = Crystal::System::Time.compute_utc_seconds_and_nanoseconds
     (seconds * 1_000 + (nanoseconds / NANOSECONDS_PER_MILLISECOND)) - EPOCH_MSECONDS_TIMESTAMP
@@ -39,12 +38,11 @@ struct Crtimestamp
 
   # Returns an utc epoch current timestamp in second as float64
   #
-  #```
+  # ```
   # Time.now_utc_to_unix_f # => 1452567845.876736
-  #```
+  # ```
   def self.now_utc_to_unix_f : Float64
     seconds, nanoseconds = Crystal::System::Time.compute_utc_seconds_and_nanoseconds
     (seconds.to_f + (nanoseconds.to_f / 1e9)) - EPOCH_SECONDS_TIMESTAMP_F64
   end
-
 end
